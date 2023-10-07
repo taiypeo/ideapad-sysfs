e97cdb4fe7efd4d4884c2f0ccc713a3a7979e1e0
taiypeo <Qwertygid@gmail.com>
2023-10-07 03:37:13 +0300

    Rework sysfs logic

--- src/sysfs.rs
+++ src/sysfs.rs
@@ -1,7 +1,7 @@
-use std::fs::{self, File, OpenOptions};
+use std::fs;
 use std::io::{self, Read, Write};
 use std::num::ParseIntError;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 // TODO: move sysfs item enums here?
 
@@ -16,13 +16,59 @@ pub enum Error {
     VPCDirectoryNotFound,
     ReadError(io::Error),
     InvalidFileContent(ParseIntError),
+    FileIsNotBinary,
     WriteError(io::Error),
 }
 
-pub fn open_file(
-    sysfs_item: &impl crate::SysfsItem,
-    open_options: &OpenOptions,
-) -> Result<File, Error> {
+enum File {
+    CameraPower,
+    ConservationMode,
+    FnLock,
+    FanMode,
+}
+
+impl File {
+    fn filename(&self) -> &'static str {
+        match self {
+            File::CameraPower => "camera_power",
+            File::ConservationMode => "conservation_mode",
+            File::FnLock => "fn_lock",
+            File::FanMode => "fan_mode",
+        }
+    }
+}
+
+enum Action {
+    On,
+    Off,
+    Set(u8),
+    Load,
+    Toggle,
+}
+
+impl Action {
+    fn perform(&self, filename: &str) -> Result<(), Error> {
+        let filepath = construct_filepath(filename)?;
+        match self {
+            Action::On => set(&filepath, 1),
+            Action::Off => set(&filepath, 0),
+            Action::Set(value) => set(&filepath, *value),
+            Action::Load => {
+                let value = load(&filepath)?;
+                println!("{}", value);
+                Ok(())
+            }
+            Action::Toggle => toggle(&filepath),
+        }
+    }
+}
+
+pub struct FileAction {
+    file: File,
+    action: Action,
+}
+
+fn construct_filepath(filename: &str) -> Result<PathBuf, Error> {
     let path: &Path = Path::new(BASE_PATH);
 
     let it = fs::read_dir(path).map_err(|err| Error::BasePathError(err))?;
@@ -36,26 +82,60 @@ pub fn open_file(
             continue;
         }
 
-        let path = path.join(dirname).join(sysfs_item.filename());
-        let file = open_options
-            .open(path)
-            .map_err(|err| Error::FileError(err))?;
-        return Ok(file);
+        return Ok(path.join(dirname).join(filename));
     }
 
     Err(Error::VPCDirectoryNotFound)
 }
 
-pub fn read_from_file(file: &mut File) -> Result<u8, Error> {
+fn open_file(path: &PathBuf, open_options: &fs::OpenOptions) -> Result<fs::File, Error> {
+    Ok(open_options
+        .open(path)
+        .map_err(|err| Error::FileError(err))?)
+}
+
+fn read_from_file(file: &mut fs::File) -> Result<u8, Error> {
     let mut buf = String::new();
     file.read_to_string(&mut buf)
         .map_err(|err| Error::ReadError(err))?;
     buf.parse().map_err(|err| Error::InvalidFileContent(err))
 }
 
-pub fn write_to_file(file: &mut File, value: u8) -> Result<(), Error> {
+fn write_to_file(file: &mut fs::File, value: u8) -> Result<(), Error> {
     let buf = value.to_string();
     Ok(file
         .write_all(buf.as_bytes())
         .map_err(|err| Error::WriteError(err))?)
+}
+
+fn set(path: &PathBuf, value: u8) -> Result<(), Error> {
+    let mut options = fs::OpenOptions::new();
+    let options = options.write(true);
+    let mut file = open_file(path, &options)?;
+    Ok(write_to_file(&mut file, value)?)
+}
+
+fn load(path: &PathBuf) -> Result<u8, Error> {
+    let mut options = fs::OpenOptions::new();
+    let options = options.read(true);
+    let mut file = open_file(path, &options)?;
+    Ok(read_from_file(&mut file)?)
+}
+
+fn toggle(path: &PathBuf) -> Result<(), Error> {
+    let mut options = fs::OpenOptions::new();
+    let options = options.read(true).write(true);
+    let mut file = open_file(path, &options)?;
+    let content = read_from_file(&mut file)?;
+
+    let mut new_content;
+    if content == 0 {
+        new_content = 1;
+    } else if content == 1 {
+        new_content = 0;
+    } else {
+        return Err(Error::FileIsNotBinary);
+    }
+
+    Ok(write_to_file(&mut file, new_content)?)
 }
